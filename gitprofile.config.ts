e5959e2d6de22ef05068bf5bd04c8a0d6ef64156
Data Jackson <b.jackson1906@gmail.com>
2024-10-07 15:32:18 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -35,12 +35,8 @@ const CONFIG = {
       {
         title: 'Sweat Equity Homes',
         description: `This is a passion project that shifts the focus of home buying more on sweat equity than move-in-ready homes. Sweat Equity Homes is a real estate web application designed to help users discover the potential value in upgrading properties by utilizing machine learning predictions to estimate possible increases in property value through renovations and improvements.\n\n
-        **Features:**\n
-        - Display detailed property information, including price, square footage, and location.\n
-        - Predict potential sweat equity based on property details using machine learning models.\n
-        - Suggest renovation projects with estimated costs and returns on investment (ROI).\n
-        - Visualize property location and surrounding amenities with interactive maps.\n
-        - View historical and future predictions of property values.\n\n
+      
+        
         **Technologies Used:**\n
         - Flask: Web framework for the backend.\n
         - Flask-SQLAlchemy: Database operations.\n
@@ -48,6 +44,11 @@ const CONFIG = {
         - APScheduler: Task scheduling (e.g., updating property data).\n
         - Leaflet.js: Map rendering and geographic visualization.\n
         - HTML/CSS: Frontend design and layout.`,
+        features: '- Display detailed property information, including price, square footage, and location.\n
+        - Predict potential sweat equity based on property details using machine learning models.\n
+        - Suggest renovation projects with estimated costs and returns on investment (ROI).\n
+        - Visualize property location and surrounding amenities with interactive maps.\n
+        - View historical and future predictions of property values.\n',
         imageUrl: '/homeequity.webp',
         link: 'https://example.com',
       },
